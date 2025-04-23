bbc5b0d4ef5eae6bf7d0a6f6ab6c26962cd2b9d2
Ashim Maskey <ashimmaskey4@gmail.com>
2025-04-23 18:59:20 +0545

    added user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,3 +1,58 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImg: {
+      type: String,
+      default:
+        "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png",
+    },
+    coverImg: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2022/12/01/04/35/sunset-7628294_1280.jpg",
+    },
+    bookmarks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Novel",
+        default: [],
+      },
+    ],
+    status: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "reader", "author"],
+      default: "reader",
+    },
+    bio: {
+      type: String,
+      default: "This user has not set a bio yet.",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
