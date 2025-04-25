c25633505c23e5a10436ddd02d8762bdb5a264cb
Ashim Maskey <ashimmaskey4@gmail.com>
2025-04-25 08:30:07 +0545

    added followunfollowuser controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -12,3 +12,51 @@ export const handleGetUserProfile = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const handleFollowUnfollowUser = async (req, res) => {
+  try {
+    const { id: recieverId } = req.params;
+    const { _id: senderId } = req.user;
+
+    const recieverUser = await User.findById(recieverId);
+    if (!recieverUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const senderUser = await User.findById(senderId);
+    if (!senderUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (recieverId === senderId.toString()) {
+      return res
+        .status(400)
+        .json({ message: "You cannot follow/unfollow yourself" });
+    }
+
+    const isFollowing = senderUser.following.includes(recieverId);
+
+    if (isFollowing) {
+      await User.findByIdAndUpdate(senderId, {
+        $pull: { following: recieverId },
+      });
+      await User.findByIdAndUpdate(recieverId, {
+        $pull: { followers: senderId },
+      });
+      return res.status(200).json({ message: "Unfollowed successfully" });
+    } else {
+      await User.findByIdAndUpdate(senderId, {
+        $addToSet: { following: recieverId },
+      });
+      await User.findByIdAndUpdate(recieverId, {
+        $addToSet: { followers: senderId },
+      });
+      return res.status(200).json({ message: "Followed successfully" });
+
+      //Todo: send notification to the reciever user
+    }
+  } catch (error) {
+    console.error("Error following/unfollowing user controller:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { handleGetUserProfile } from "../controllers/user.controller.js";
+import {
+  handleFollowUnfollowUser,
+  handleGetUserProfile,
+} from "../controllers/user.controller.js";
+import verifyToken from "../middlewares/verifyToken.js";
 const router = express.Router();
 
 router.get("/profile/:username", handleGetUserProfile);
+router.patch("/followUnfollow/:id", verifyToken, handleFollowUnfollowUser);
 
 export default router;
