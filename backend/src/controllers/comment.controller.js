d7f27248c4232c50ce82056f9b7f5b755e91d6e3
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-05 16:14:32 +0545

    added getcommentbychapter and novel controller

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,4 +1,6 @@
-import Comment from "../models/comment.model";
+import Chapter from "../models/chapter.model.js";
+import Comment from "../models/comment.model.js";
+import Novel from "../models/novel.model.js";
 import User from "../models/user.model.js";
 
 export const handleCreateComment = async (req, res) => {
@@ -15,7 +17,7 @@ export const handleCreateComment = async (req, res) => {
     if (content.length < 5)
       return res
         .status(400)
-        .json({ message: "Comment should be at least 5 character" });
+        .json({ message: "Comment should be at least 5 characters long" });
 
     const comment = await Comment.create({
       user: userId,
@@ -34,6 +36,45 @@ export const handleCreateComment = async (req, res) => {
     return res.status(201).json(populatedComment);
   } catch (error) {
     console.error("Error creating comment controller", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const handleGetCommentOnChapter = async (req, res) => {
+  try {
+    const chapterId = req.params.id;
+    const chapter = await Chapter.findById(chapterId);
+    if (!chapter) return res.status(404).json({ message: "Chapter not found" });
+
+    const comments = await Comment.find({ chapter: chapterId })
+      .populate("user", "username profileImg")
+      .sort({ createdAt: -1 });
+
+    if (comments.length === 0)
+      return res.status(404).json({ message: "No comments yet!" });
+
+    return res.status(200).json(comments);
+  } catch (error) {
+    console.error("Error getting all comments in chapter  controller", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const handleGetCommentOnNovel = async (req, res) => {
+  try {
+    const novelId = req.params.id;
+    const novel = await Novel.findById(novelId);
+    if (!novel) return res.status(404).json({ message: "Novel not found" });
+
+    const comments = await Comment.find({ novel: novelId })
+      .populate("user", "username profileImg")
+      .sort({ createdAt });
+
+    if (comments.length === 0)
+      return res.status(404).json({ message: "No comments yet!" });
+    return res.status(200).json(comments);
+  } catch (error) {
+    console.error("Error getting all comments in novel controller", error);
     return res.status(500).json({ message: "Internal server error" });
   }
 };
