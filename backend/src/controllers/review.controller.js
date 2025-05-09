9d43fa52fd4ae8099154f0a0ec1f2c6866c7064e
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-09 22:50:43 +0545

    added create review controller

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -1,7 +1,60 @@
 import Novel from "../models/novel.model.js";
 import Review from "../models/review.model.js";
 
-export const handleCreateReview = async (req, res) => {};
+export const handleCreateReview = async (req, res) => {
+  try {
+    const novelId = req.params.id;
+    const novel = await Novel.findById(novelId);
+    if (!novel) {
+      return res.status(404).json({ message: "Novel not found" });
+    }
+
+    const userId = req.user._id;
+
+    const existingReview = await Review.findOne({
+      novel: novelId,
+      user: userId,
+    });
+    if (existingReview) {
+      return res
+        .status(400)
+        .json({ message: "You have already reviewed this novel" });
+    }
+
+    const { rating, review } = req.body;
+    if (!rating || !review) {
+      return res
+        .status(400)
+        .json({ message: "Rating and review are required" });
+    }
+
+    if (rating < 1 || rating > 5) {
+      return res
+        .status(400)
+        .json({ message: "Rating must be between 1 and 5" });
+    }
+    if (review.length < 10 || review.length > 200) {
+      return res
+        .status(400)
+        .json({ message: "Review must be between 10 and 200 characters" });
+    }
+
+    const newReview = await Review.create({
+      novel: novelId,
+      user: userId,
+      rating,
+      review,
+    });
+    if (!newReview) {
+      return res.status(400).json({ message: "Failed to create review" });
+    }
+    return res.status(201).json(newReview);
+  } catch (error) {
+    console.error("Error creating review controller:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const handleUpdateReview = async (req, res) => {};
 
 export const handleGetReview = async (req, res) => {
@@ -23,7 +76,7 @@ export const handleGetReview = async (req, res) => {
     return res.status(200).json(reviews);
   } catch (error) {
     console.error("Error getting reviews controller:", error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -49,6 +102,6 @@ export const handleDeleteReview = async (req, res) => {
     return res.status(200).json(deletedReview);
   } catch (error) {
     console.error("Error deleting review controller:", error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
