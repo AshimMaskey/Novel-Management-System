d571e782804d8e98587b306fe28ab8470e881e72
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-09 22:25:39 +0545

    added get review controller

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -1,4 +1,28 @@
+import Novel from "../models/novel.model.js";
+import Review from "../models/review.model.js";
+
 export const handleCreateReview = async (req, res) => {};
 export const handleUpdateReview = async (req, res) => {};
-export const handleGetReview = async (req, res) => {};
+export const handleGetReview = async (req, res) => {
+  try {
+    const novelId = req.params.id;
+    const novel = await Novel.findById(novelId);
+    if (!novel) {
+      return res.status(404).json({ message: "Novel not found" });
+    }
+
+    const reviews = await Review.find({ novel: novelId })
+      .populate("user", "username profileImg")
+      .sort({ createdAt: -1 });
+    if (reviews.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No reviews found for this novel" });
+    }
+    return res.status(200).json(reviews);
+  } catch (error) {
+    console.error("Error getting reviews controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 export const handleDeleteReview = async (req, res) => {};
--- backend/src/routes/review.route.js
+++ backend/src/routes/review.route.js
@@ -4,6 +4,7 @@ import checkId from "../middlewares/checkId";
 import {
   handleCreateReview,
   handleDeleteReview,
+  handleGetReview,
   handleUpdateReview,
 } from "../controllers/review.controller.js";
 const router = express.Router();
@@ -12,8 +13,7 @@ router
   .route("/:id")
   .post(verifyToken, checkId, handleCreateReview)
   .delete(verifyToken, checkId, handleDeleteReview)
-  .patch(verifyToken, checkId, handleUpdateReview);
-
-router.route("/").get(handleGetReview);
+  .patch(verifyToken, checkId, handleUpdateReview)
+  .get(checkId, handleGetReview);
 
 export default router;
