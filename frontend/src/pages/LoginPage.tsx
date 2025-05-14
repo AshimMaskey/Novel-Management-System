108286b0526913bab04b9c883fcc38d93f9474d1
Ashim Maskey <ashimmaskey4@gmail.com>
2025-05-14 22:41:45 +0545

    added handlesubmit and error

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,36 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+interface LoginData {
+  username: string;
+  password: string;
+}
+
 const LoginPage = () => {
+  const [error, setError] = useState<string | null>(null);
+  const [formData, setFormData] = useState<LoginData>({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      setError("Please fill in all fields.");
+      return;
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-center h-screen">
@@ -11,7 +39,7 @@ const LoginPage = () => {
             <CardTitle>Login</CardTitle>
           </CardHeader>
           <CardContent>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="mb-5">
                 <Label className="mb-3 text-lg" htmlFor="username">
                   Username:
@@ -20,10 +48,15 @@ const LoginPage = () => {
                   className="border-2 p-5 border-border"
                   type="text"
                   id="username"
+                  name="username"
                   placeholder="Enter your username..."
+                  value={formData.username}
+                  onChange={(e) =>
+                    setFormData({ ...formData, username: e.target.value })
+                  }
                 />
               </div>
-              <div>
+              <div className="mb-2">
                 <Label className="mb-3 text-lg" htmlFor="password">
                   Password:
                 </Label>
@@ -31,14 +64,29 @@ const LoginPage = () => {
                   className="border-2 p-5 border-border"
                   type="password"
                   id="password"
+                  name="password"
                   placeholder="Enter your password..."
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
                 />
               </div>
-              <Button type="submit" className="w-full mt-5">
+              <span className="text-destructive">{error}</span>
+              <Button type="submit" className="w-full mt-3">
                 Login
               </Button>
             </form>
           </CardContent>
+          <CardFooter>
+            <p>
+              Doesn't have an account?
+              <span className="underline text-primary">
+                {" "}
+                <Link to="/signup">Sign Up</Link>
+              </span>
+            </p>
+          </CardFooter>
         </Card>
       </div>
     </>
