528e4b293a08b342bff72230415d6cb6396d4234
Ashim Maskey <ashimmaskey4@gmail.com>
2025-07-02 20:31:00 +0545

    added genreapi

---
+++ frontend/src/features/genre/genreApi.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const genreApi = createApi({
+  reducerPath: "genreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${import.meta.env.VITE_API_BASE_URL}/genre`,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    fetchGenre: builder.query({
+      query: () => ({
+        url: "/",
+        method: "GET",
+      }),
+    }),
+    createGenre: builder.mutation({
+      query: (genreName) => ({
+        url: "/",
+        method: "POST",
+        body: genreName,
+      }),
+    }),
+    deleteGenre: builder.mutation({
+      query: (id) => ({
+        url: `/${id}`,
+        method: "DELETE",
+      }),
+    }),
+    editGenre: builder.mutation({
+      query: ({ id, genreName }) => ({
+        url: `/${id}`,
+        method: "PATCH",
+        body: genreName,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateGenreMutation,
+  useDeleteGenreMutation,
+  useEditGenreMutation,
+  useFetchGenreQuery,
+} = genreApi;
