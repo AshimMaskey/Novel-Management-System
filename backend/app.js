44967862c55b9d7e93cb4fac6a64463b07756b22
Ashim Maskey <ashimmaskey4@gmail.com>
2025-08-14 10:34:06 +0545

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://novel-management-system-frontend.vercel.app/",
+      "https://novel-management-system-frontend.vercel.app",
     ],
     credentials: true,
   })
